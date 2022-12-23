0632e41e4a27e3bb0fca66b1135c2adbf7318992
Pedenite <pedenite@gmail.com>
2022-12-23 20:02:06 -0300

    15 - no more mocks

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollView, View } from "react-native";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import Foundation from "react-native-vector-icons/Foundation";
@@ -7,52 +7,48 @@ import { normalizeString, verifyStringInclusion } from "../../helpers/stringMana
 
 import { TouchableOpacity } from "react-native-gesture-handler";
 import styles from "./styles";
-
-const mockTopics = [
-    {
-        name: "Ciência de Dados",
-        icon: "graph-bar"
-    },
-    {
-        name: "Computação em Nuvem",
-        icon: "cloud"
-    },
-    {
-        name: "Aprendizado de Máquina",
-        icon: "graph-bar"
-    }
-];
-
-const mockPersonsRank = [
-    {
-        name: "user 1",
-        points: "180"
-    },
-    {
-        name: "user 2",
-        points: "2"
-    },
-    {
-        name: "user 3",
-        points: "1802"
-    },
-    {
-        name: "user 4",
-        points: "15"
-    }
-];
+import TopicsCollection from "../../services/firebase/db/topics";
+import UsersCollection from "../../services/firebase/db/users";
 
 export function Home({ navigation }) {
-    const [searchPhrase, setSearchPhrase] = useState("");
+    const TOPICS_LIMIT = 3;
+    const RANKING_LIMIT = 10;
 
-    const topicsList = mockTopics
+    const [searchPhrase, setSearchPhrase] = useState("");
+    const [topics, setTopics] = useState([]);
+    const [people, setPeople] = useState([]);
+
+    const parseCollection = (info) => {
+        const arr = [];
+
+        info.forEach((doc) => {
+            arr.push({ id: doc.id, ...doc.data() });
+        });
+
+        return arr;
+    }
+
+    useEffect(() => {
+        TopicsCollection.getAll().then((topicsInfo) => {
+            setTopics(parseCollection(topicsInfo));
+        });
+    }, []);
+
+    useEffect(() => {
+        UsersCollection.getAll().then((usersInfo) => {
+            setPeople(parseCollection(usersInfo));
+        });
+    }, []);
+
+    const topicsList = topics
         .filter((topic) => verifyStringInclusion(normalizeString(topic.name), normalizeString(searchPhrase)))
         .map((topic, index) => (
             <View style={styles.topicsCard} key={index}>
                 <Foundation name={topic.icon} size={40} color="white" />
                 <Text style={styles.topicName}>{topic.name}</Text>
             </View>
-        ));
+        ))
+        .slice(0, TOPICS_LIMIT);
 
     return (
         <Wrapper>
@@ -81,7 +77,7 @@ export function Home({ navigation }) {
                             <AntDesign name="arrowsalt" size={12} color="white" />
                         </View>
 
-                        {mockPersonsRank.map((person, index) => (
+                        {people.slice(0, RANKING_LIMIT).map((person, index) => (
                             <UserCardSimple user={person} key={index} />
                         ))}
                     </View>
