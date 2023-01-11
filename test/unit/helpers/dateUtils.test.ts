5071a6890da05a098acd0d9d668fc325a9d30613
Pedenite <pedenite@gmail.com>
2023-01-11 20:15:02 -0300

    34 - preparing tests

--- test/unit/helpers/dateUtils.test.ts
+++ test/unit/helpers/dateUtils.test.ts
@@ -1,4 +1,4 @@
-import { getDateFromSeconds } from "./dateUtils";
+import { getDateFromSeconds } from "../../../src/helpers/dateUtils";
 
 test('dateUtils works accordingly', () => {
     expect(getDateFromSeconds(1672531200)).toEqual(new Date(2023, 0, 1));
