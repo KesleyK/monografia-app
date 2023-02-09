8c2fb63e89c2818e545301ec4c7597389c956d1a
Pedenite <pedenite@gmail.com>
2023-02-09 20:40:25 -0300

    74 - white mode

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,17 +1,35 @@
+// export enum DefaultStyles {
+//     PRIMARY_COLOR = "#EEE",
+//     SECONDARY_COLOR = "#727272",
+//     TERTIARY_COLOR = "#555",
+//     BACKGROUND_COLOR = "#2B2B2B",
+//     CARD_COLOR = "#363E40",
+//     SEARCH_BAR_COLOR = "#EEE",
+//     ANCHOR_COLOR = "#30A7C1",
+//     PLACEHOLDER_COLOR = "#363E40",
+//     BUTTON_COLOR = "#EEE",
+//     BUTTON_TEXT_COLOR = "#000",
+//     DISABLED_COLOR = "#727272",
+//     ERROR_COLOR = "#F86262",
+//     ERROR_COLOR_LIGHT = "#F1CBCB",
+//     SUCCESS_COLOR = "#5CB85C",
+//     ICON_COLOR = "white"
+// }
+
 export enum DefaultStyles {
-    PRIMARY_COLOR = "#EEE",
-    SECONDARY_COLOR = "#727272",
-    TERTIARY_COLOR = "#555",
-    BACKGROUND_COLOR = "#2B2B2B",
-    CARD_COLOR = "#363E40",
-    SEARCH_BAR_COLOR = "#EEE",
+    PRIMARY_COLOR = "#2B2B2B",
+    SECONDARY_COLOR = "#999",
+    TERTIARY_COLOR = "#aaa",
+    BACKGROUND_COLOR = "#eee",
+    CARD_COLOR = "#ccc",
+    SEARCH_BAR_COLOR = "#ddd",
     ANCHOR_COLOR = "#30A7C1",
-    PLACEHOLDER_COLOR = "#363E40",
-    BUTTON_COLOR = "#EEE",
-    BUTTON_TEXT_COLOR = "#000",
-    DISABLED_COLOR = "#727272",
+    PLACEHOLDER_COLOR = "#ccc",
+    BUTTON_COLOR = "#2B2B2B",
+    BUTTON_TEXT_COLOR = "#fff",
+    DISABLED_COLOR = "#ddd",
     ERROR_COLOR = "#F86262",
     ERROR_COLOR_LIGHT = "#F1CBCB",
     SUCCESS_COLOR = "#5CB85C",
-    ICON_COLOR = "white"
+    ICON_COLOR = "black"
 }
